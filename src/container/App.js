c7deeb21bd1002cda42aed054853554064d63d61
MehdiZandian <mahdizandi81@gmail.com>
2022-05-02 18:55:56 +0430

    fix height bug on mobile (2) 🔨

--- src/container/App.js
+++ src/container/App.js
@@ -8,7 +8,7 @@ function App() {
   const mode = useSelector((state) => state.mode.mode);
   const user = useSelector((state) => state.user.user);
   return (
-    <div className="app h-screen min-h-[710px] bg-center bg-cover bg-no-repeat bg-[url('./assets/mountain.jpg')] ">
+    <div className="app h-screen min-h-[770px] md:min-h-[710px] bg-center bg-cover bg-no-repeat bg-[url('./assets/mountain.jpg')] ">
       <div className={`${mode == true ? "dark" : "light"} bg-gradient h-full`}>
         <Nav />
         {user.length === 0 ? <Form /> : <Welcome />}
